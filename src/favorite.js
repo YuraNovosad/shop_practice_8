65cddc57bae709c69da7f88ea2224cd52b966239
YuraNovosad <yuranovosad@gmail.com>
2023-05-25 21:29:09 +0300

    виношу функції в окремі файли

--- src/favorite.js
+++ src/favorite.js
@@ -1,6 +1,22 @@
 import { common } from './common';
 import { createMarckup } from './helper/createMarckup';
+import { createModal } from './helper/createModal';
+import { instruments } from './helper/instruments';
 
 const list = document.querySelector('.js-list');
 const favorite = JSON.parse(localStorage.getItem(common.KEY_FAVORITE)) ?? [];
 createMarckup(favorite, list);
+list.addEventListener('click', onClick);
+
+function onClick(evt) {
+  evt.preventDefault();
+  if (evt.target.classList.contains('js-info')) {
+    const product = findProduct(evt.target);
+    createModal(product);
+  }
+}
+
+function findProduct(elem) {
+  const productId = Number(elem.closest('.js-card').dataset.id);
+  return instruments.find(({ id }) => id === productId);
+}
